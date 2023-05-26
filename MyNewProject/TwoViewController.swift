619e3723cc84fc2308df523e69b90f5f39bb2a93
v.sakaev <v.sakaev@qvant.ru>
2023-05-26 14:07:24 +0500

    added two screen

---
+++ MyNewProject/TwoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TwoViewController.swift
+//  MyNewProject
+//
+//  Created by Sakaev Vladimir on 26.05.2023.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func sendEventBtn(_ sender:Any){
+        
+    }
+    
+    @IBAction func backBtn(_ sender: Any) {
+        
+    }
+    
+}
